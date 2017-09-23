76952234b29421b9a94bb8488158828b63965f8e
NiyazSafiullin <s.niyaz@mail.ru>
2017-09-23 10:42:57 +0300

    11

---
+++ Tasks/Task2/Program.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Program {
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		int x=scanner.nextInt();
+		int y=scanner.nextInt();
+		int sumresult= x+y;
+		System.out.println(sumresult);
+		System.out.println("111");
+		System.out.println("1211");
+		System.out.println("1211");
+	}
+}
