d946090b34efa5e01a22401a98c76f928f202eef
NiyazSafiullin <s.niyaz@mail.ru>
2017-09-23 12:23:46 +0300

    r

--- Tasks/Task2/Program.java
+++ Tasks/Task2/Program.java
@@ -6,7 +6,7 @@ class Program {
 		int y=scanner.nextInt();
 		int sumresult= x+y;
 		System.out.println(sumresult);
-		System.out.println("111");
+		System.out.println("198711");
 		System.out.println("1211");
 		System.out.println("1sss211");
 	}
