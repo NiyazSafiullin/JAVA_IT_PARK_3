56658aa7f52e737df7337c28f2f98c0b8602180f
NiyazSafiullin <s.niyaz@mail.ru>
2017-10-08 17:45:53 +0300

    homework5

--- Tasks/HomeWork/Task5/src/com/company/Document.java
+++ Tasks/HomeWork/Task5/src/com/company/Document.java
@@ -5,15 +5,34 @@ import java.util.Scanner;
 public class Document {
     int count = 0;
     int temp=0;
-    String[] Lines = new String[9];
+    String[][] Lines = new String[4][4];
 
 
     void showDocuments() {
-        for (int i = 0; i < Lines.length; i++) {
-            String lineToOut = Lines[i] == null ? "_______" : Lines[i];
-            System.out.println(i + ": " + lineToOut);
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Документ " + i);
+            for (int j = 0; j < 4; j++) {
+                String lineToOut = Lines[i][j] == null ? "_______" : Lines[i][j];
+                System.out.println(j + ": " + lineToOut);
+            }
         }
     }
+    void showDocuments(Integer str) {
+        for (int j = 0; j < 4; j++) {
+
+            String lineToOut = Lines[str][j] == null ? "_______" : Lines[str][j];
+            System.out.println(j + ": " + lineToOut);
+        }
+    }
+
+    void addLineToBegin(String newLine, Integer str) {
+        for (int j = count; j > 0; j--) {
+            Lines[str][j] = Lines[str][j - 1];
+        }
+        Lines[str][0] = newLine;
+        count++;
+    }
+
 
     void clearDocuments() {
         for (int i = 0; i < Lines.length; i++) {
@@ -31,7 +50,7 @@ public class Document {
                 Lines[i] = Lines[i - 1];
             }
         }
-        Lines[0] = rez;
+      //  Lines[0] = rez;
         count++;
     }
 
@@ -43,7 +62,7 @@ public class Document {
            if (Lines[i]==null) {
                Lines[i]=Lines[i+1];
            }
-Lines[i]=rez;
+//Lines[i]=rez;
         }
 
         count++;
@@ -70,7 +89,7 @@ Lines[i]=rez;
         int vibor = scanner.nextInt();
         for (int i = 0; i < Lines.length; i++) {
             if (i == vibor) {
-                Lines[i] = zamena;
+      //          Lines[i] = zamena;
             }
 
             count++;
