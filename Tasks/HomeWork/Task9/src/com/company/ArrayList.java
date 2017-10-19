69a950ef3dc27d763f77ae50136b53ea27dd28ef
NiyazSafiullin <s.niyaz@mail.ru>
2017-10-19 21:37:06 +0300

    homework9

---
+++ Tasks/HomeWork/Task9/src/com/company/ArrayList.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class ArrayList implements List {
+    private int count=0;
+    int [] array= new int [5];
+
+    @Override
+    public void add(int a) {
+
+    }
+
+    @Override
+    public void delete(int a) {
+
+    }
+
+    @Override
+    public void addindex() {
+
+    }
+
+    @Override
+    public void returns() {
+
+    }
+
+    @Override
+    public void search() {
+
+    }
+}
---
+++ Tasks/HomeWork/Task9/src/com/company/List.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public interface List {
+    void add (int a);
+    void delete (int a);
+    void search();
+    void addindex();
+    void returns();
+
+
+}
