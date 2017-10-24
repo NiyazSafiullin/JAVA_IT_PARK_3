f4830bb28ffd2dd3b2c8c3010de3a346aea04122
NiyazSafiullin <s.niyaz@mail.ru>
2017-10-24 23:27:48 +0300

    classwork

--- Tasks/HomeWork/Task10/src/com/company/Main.java
+++ Tasks/HomeWork/Task10/src/com/company/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Car car= Car.builder()
                 .model("opel").year(2013).max_speed(200).build();
-        
+
     }
 }
