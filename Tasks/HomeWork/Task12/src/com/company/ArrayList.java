88949bcf5861bc69b0bf24e884aefc46640562dc
NiyazSafiullin <s.niyaz@mail.ru>
2017-10-30 13:48:41 +0300

    homework

--- Tasks/HomeWork/Task12/src/com/company/ArrayList.java
+++ Tasks/HomeWork/Task12/src/com/company/ArrayList.java
@@ -1,7 +1,19 @@
 package com.company;
 
 public class ArrayList {
+private int count=0;
+private Object arrayList[];
+
+
+    public ArrayList() {
+        this.count = 0;
+        this.arrayList = new Object[100];
+    }
 
 
 
+    public ArrayList(int count, Object[] arrayList) {
+        this.count = count;
+        this.arrayList = arrayList;
+    }
 }
--- Tasks/HomeWork/Task12/src/com/company/Main.java
+++ Tasks/HomeWork/Task12/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DataReader reader = new DataReader("input.txt");
+        String word = reader.readString();
+        DataReader numberReader = new DataReader("input.txt");
+        int number = numberReader.readInteger();
+
+        List ageList[] = new LinkedList[100];
+        for (int i = 0; i < 100; i++) {
+            ageList[i] = new LinkedList();
+        }
     }
 }
